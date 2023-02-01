22f50f4c591d26a8fb9e01f12bd2ddfd7e84de0e
Pat Monette <patrick@ontarioseo.ca>
2023-02-01 16:43:40 +0000

    card block updates

--- global-templates/blocks/block-card.php
+++ global-templates/blocks/block-card.php
@@ -3,10 +3,11 @@ $htmltag = get_field('title_html_tag');
 $htmltagopen = "<" . $htmltag . " class='card-title'>";
 $htmltagclose = "</" . $htmltag . ">";
 $icon = get_field('icon');
+$no_min = get_field('no_min_height');
 ?>
-<div class="shadowbox card <?php if(get_field('card_link')): ?>hover<?php endif; ?>">
+<div class="shadowbox card <?php if($no_min == 1) { echo 'no-min-height'; } ?> <?php if(get_field('card_link')) { echo 'hover'; } ?>">
 	<?php if($icon): echo wp_get_attachment_image($icon, 'full'); endif; ?>
 	<?php echo $htmltagopen; ?><?php if(get_field('card_link')): ?><a href="<?php the_field('card_link'); ?>" class="stretched-link"><?php endif; the_field('card_title'); if(get_field('card_link')): ?></a><?php endif; ?><?php echo $htmltagclose; ?>
-	<p class="card-subtitle"><?php the_field('card_subtitle'); ?></p>
-	<p class="card-content"><?php the_field('card_content'); ?></p>
+	<?php if(get_field('card_subtitle')): ?><p class="card-subtitle"><?php the_field('card_subtitle'); ?></p><?php endif; ?>
+	<?php if(get_field('card_content')): ?><p class="card-content"><?php the_field('card_content'); ?></p><?php endif; ?>
 </div>
