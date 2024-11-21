19e4d9db133765a3ef176427417b96260fb21458
Pat Monette <patrick@ontarioseo.ca>
2024-11-21 14:17:33 -0500

    fix block card image margin

--- global-templates/blocks/block-card.php
+++ global-templates/blocks/block-card.php
@@ -6,7 +6,7 @@ $icon = get_field('icon');
 $no_min = get_field('no_min_height');
 ?>
 <div class="shadowbox card <?php if($no_min == 1) { echo 'no-min-height'; } else { echo 'h-100'; } ?> <?php if(get_field('card_link')) { echo 'hover'; } ?>">
-	<?php if($icon): echo wp_get_attachment_image($icon, 'full'); endif; ?>
+	<?php if($icon): echo wp_get_attachment_image($icon, 'full', '', array('class' => 'mb-2')); endif; ?>
 	<?php echo $htmltagopen; ?><?php if(get_field('card_link')): ?><a href="<?php echo get_field('card_link'); ?>" class="stretched-link"><?php endif; echo get_field('card_title'); if(get_field('card_link')): ?></a><?php endif; ?><?php echo $htmltagclose; ?>
 	<?php if(get_field('card_subtitle')): ?><p class="card-subtitle h-auto"><?php echo get_field('card_subtitle'); ?></p><?php endif; ?>
 	<?php if(get_field('card_content')): ?><p class="card-content mt-auto"><?php echo get_field('card_content'); ?></p><?php endif; ?>
